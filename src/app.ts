e8f567d48f3e3503418147a0440604a4b4e75c73
hieunq <hieunq@ows.vn>
2019-05-15 11:49:21 +0700

    add phaser

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
 import Player from "./controller/player";
+import "phaser"
 
 const player = new Player("marverlous");
 console.log("hello ", player.getName());
+
+const config: Phaser.Types.Core.GameConfig = {
+    title: "Starfall",
+    width: 800,
+    height: 600,
+    parent: "game",
+    backgroundColor: "#18216D"
+};
+
+const game = new Phaser.Game(config);
